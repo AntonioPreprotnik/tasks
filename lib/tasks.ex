cf512e4d701ac5ba8b06a1e5aa487eb2163a642e
TheGamePrep <preprotnik.novi@gmail.com>
2021-05-06 12:50:37 +0200

    Implement fizz_buzz

--- lib/tasks.ex
+++ lib/tasks.ex
@@ -1,2 +1,14 @@
 defmodule Tasks do
+  def fizz_buzz(n) when n < 1 or n > 100 do
+    n
+  end
+
+  def fizz_buzz(n) do
+    case {rem(n, 3) == 0, rem(n, 5) == 0} do
+      {false, true} -> "Buzz"
+      {true, false} -> "Fizz"
+      {true, true} -> "FizzBuzz"
+      {false, false} -> n
+    end
+  end
 end
