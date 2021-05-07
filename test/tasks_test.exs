917fc75f6f899c09e56bf145dce6bc53d52a0a39
TheGamePrep <preprotnik.novi@gmail.com>
2021-05-07 12:43:58 +0200

    Added tests for find first

--- test/tasks_test.exs
+++ test/tasks_test.exs
@@ -1,8 +1,18 @@
-defmodule TasksTest do
+defmodule TaskTsest do
   use ExUnit.Case
-  doctest Tasks
+  import Tasks
 
-  test "greets the world" do
-    assert Tasks.hello() == :world
+  describe "Nomatch" do
+    test "return no solution" do
+      assert "no solution" == find_first("aabb")
+    end
+
+    test "return a for wawer" do
+      assert "a" == find_first("wawer")
+    end
+
+    test "return b for abanox" do
+      assert "b" == find_first("abanox")
+    end
   end
 end
