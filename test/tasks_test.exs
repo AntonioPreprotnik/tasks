cf6c0c205892e66d5d2c7b430067a452169aecc7
TheGamePrep <preprotnik.novi@gmail.com>
2021-05-07 18:17:25 +0200

    Test fib

--- test/tasks_test.exs
+++ test/tasks_test.exs
@@ -1,8 +1,27 @@
 defmodule TasksTest do
   use ExUnit.Case
-  doctest Tasks
 
-  test "greets the world" do
-    assert Tasks.hello() == :world
+  import Tasks
+
+  describe "fib" do
+    test "no 0th fibonacci" do
+      assert "There is no 0th fibonacci" == fib(0)
+    end
+
+    test "first fibonacci" do
+      assert 0 == fib(1)
+    end
+
+    test "second fibonacci" do
+      assert 1 == fib(2)
+    end
+
+    test "fifth fibonacci" do
+      assert 3 == fib(5)
+    end
+
+    test "21th fibonacci" do
+      assert 6765 == fib(21)
+    end
   end
 end
