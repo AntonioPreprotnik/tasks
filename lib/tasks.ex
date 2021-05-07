716dc45b57b3a0281ec77906786fbf7c4fb8602a
TheGamePrep <preprotnik.novi@gmail.com>
2021-05-07 14:51:19 +0200

    created count words

--- lib/tasks.ex
+++ lib/tasks.ex
@@ -1,9 +1,23 @@
 defmodule Tasks do
-  def fib(n) do
-    case n do
-      0 -> 0
-      1 -> 1
-      n -> fib(n - 1) + fib(n - 2)
-    end
+  def frequencies(string) do
+    list =
+      string
+      |> String.split()
+      |> Enum.map(fn word -> String.downcase(word) end)
+
+    frequencies_helper(list, %{})
+  end
+
+  defp frequencies_helper([], solution), do: solution
+
+  defp frequencies_helper([_head | []], solution), do: solution
+
+  defp frequencies_helper([head | tail], solution) do
+    head = head
+
+    count = Enum.count(tail, fn word -> word == head end) + 1
+    tail = Enum.filter(tail, fn word -> word != head end)
+    solution = Map.put(solution, head, count)
+    frequencies_helper(tail, solution)
   end
 end
--- test/tasks_test.exs
+++ test/tasks_test.exs
@@ -1,18 +1,4 @@
 defmodule TaskTsest do
   use ExUnit.Case
   import Tasks
-
-  describe "testing fibonacci" do
-    test "fib(0)" do
-      assert 0 = fib(0)
-    end
-
-    test "fib(1)" do
-      assert 1 = fib(1)
-    end
-
-    test "fib(5)" do
-      assert 5 = fib(5)
-    end
-  end
 end
