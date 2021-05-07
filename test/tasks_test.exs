f00b23d8f755e1cb5f7470b0ec6281649f24bfbd
TheGamePrep <preprotnik.novi@gmail.com>
2021-05-07 18:33:40 +0200

    Add test for frequencies

--- test/tasks_test.exs
+++ test/tasks_test.exs
@@ -1,8 +1,14 @@
 defmodule TasksTest do
   use ExUnit.Case
-  doctest Tasks
+  import Tasks
 
-  test "greets the world" do
-    assert Tasks.hello() == :world
+  describe "frequencies" do
+    test "ping" do
+      result = frequencies("ping PING PiNg pInG")
+
+      assert Map.has_key?(result, "ping")
+      refute Map.has_key?(result, "PING")
+      assert 4 == Map.get(result, "ping")
+    end
   end
 end
