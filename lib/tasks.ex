77f3c1f873c7bb151febafaa9cbf8c31255674ab
TheGamePrep <preprotnik.novi@gmail.com>
2021-05-17 18:37:50 +0200

    Refactor frequencies

--- lib/tasks.ex
+++ lib/tasks.ex
@@ -1,23 +1,8 @@
 defmodule Tasks do
   def frequencies(string) do
-    list =
-      string
-      |> String.split()
-      |> Enum.map(fn word -> String.downcase(word) end)
-
-    frequencies_helper(list, %{})
-  end
-
-  defp frequencies_helper([], solution), do: solution
-
-  defp frequencies_helper([_head | []], solution), do: solution
-
-  defp frequencies_helper([head | tail], solution) do
-    head = head
-
-    count = Enum.count(tail, fn word -> word == head end) + 1
-    tail = Enum.filter(tail, fn word -> word != head end)
-    solution = Map.put(solution, head, count)
-    frequencies_helper(tail, solution)
+    string
+    |> String.downcase()
+    |> String.split()
+    |> Enum.frequencies()
   end
 end
